c1d8e4a35ce384579e78796079eaa7bfd7ae4398
mateussuzuki <mateussuzuki2022@hotmail.com>
2024-05-15 16:03:14 -0300

    navbar

--- building-react/src/Home.jsx
+++ building-react/src/Home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
         <Navbar />
         <div className="backLettersHome">
           <div className="backLettersHomeFlex">
-            <h1 className="text-center fontColor">
+            <h1 className="text-center">
               " whatever good things we build end up building us "
             </h1>
-            <p className="mt-4 fontColor">BY JIM RHON</p>
+            <p className="mt-4">BY JIM RHON</p>
             <button className="btn px-4 py-2 mt-4">GET A QUOTES</button>
           </div>
         </div>
--- building-react/src/Navbar.jsx
+++ building-react/src/Navbar.jsx
@@ -1,28 +1,44 @@
 import React from "react";
-import '../src/css/navbar.scss'
+import "../src/css/navbar.scss";
 
 export const Navbar = () => {
   return (
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary mt-4 p-0">
-        <div class="container-fluid white p-2">
-          <ul className="navbar-nav">
+        <div class="container-fluid white p-2 d-flex justify-content-between">
+          <span class="navbar-brand mb-0 h1 ms-5">
+            <h5 className="m-0">Kons</h5>
+          </span>
+          <ul className="navbar-nav me-5">
             <li className="nav-item">
-              <a className="nav-link active" href="#">Home</a>
+              <a className="nav-link active" href="#">
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Works</a>
+              <a className="nav-link" href="#">
+                Works
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">News</a>
+              <a className="nav-link" href="#">
+                News
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="#">
+                About
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              <a className="nav-link" href="#">
+                Contact
+              </a>
             </li>
           </ul>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </nav>
     </div>
